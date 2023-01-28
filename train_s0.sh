092041ca1a4d5209c0763227677c5d51913d4078
max810 <maksym.bekuzarov@gmail.com>
2023-01-28 14:54:30 +0400

    Had to still reduce batch size to avoid crashing

--- train_s0.sh
+++ train_s0.sh
@@ -1,9 +1,9 @@
 STAGE=0
-B=6 # original 16
-LR=3.75e-06 # original 1e-5 / (16/6) batch size difference 
-START_WARM=53000 # original 20K iterations * (16/6) batch size difference 
-END_WARM=186000 # original 70K iterations * (16/6) batch size difference
-NUM_ITER=400000 # original 150K iterations * (16/6) batch size difference 
+B=4 # original 16
+LR=2.5e-06 # original 1e-5 / (16/4) batch size difference 
+START_WARM=80000 # original 20K iterations * (16/4) batch size difference 
+END_WARM=280000 # original 70K iterations * (16/4) batch size difference
+NUM_ITER=600000 # original 150K iterations * (16/4) batch size difference 
 NUM_WORKERS=6
 
 OMP_NUM_THREADS=6 python -m torch.distributed.run --master_port 25763 --nproc_per_node=2 train.py \

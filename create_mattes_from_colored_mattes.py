4f9270b5d072fbd8da1dce7b79604865e22af610
marianne.gorczyca <marianne.gorczyca@magnopus.com>
2024-02-13 09:41:02 -0800

    Make the csv file a parameter

--- create_mattes_from_colored_mattes.py
+++ create_mattes_from_colored_mattes.py
@@ -10,28 +10,21 @@ import argparse
 matte_char_to_color = {}
 
 
-def populate_char_to_color_dict():
-    """From the csv file mapping XMem2 matte colors to shot elements, create a dictionary with the elements as keys. Note the BGR colors for OpenCV."""
-    # TODO: change the name of the google sheet to matte instead of cutout   
-    csv_file_path = r'X:\project\csv_files\xmem_cutout_map.csv'
+def populate_char_to_color_dict(csv_file_path):
+    """
+    From the csv file mapping XMem2 matte colors to shot elements, create a dictionary with the elements as keys.
+    Note the BGR colors for OpenCV.
+    """
+    if os.path.exists(csv_file_path):
+        with open(csv_file_path, 'r', newline='') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
 
-    with open(csv_file_path, 'r', newline='') as csvfile:
-        csv_reader = csv.DictReader(csvfile)
-
-        for row in csv_reader:
-            element = row['Element'].lower().replace(" ", "")
-            matte_char_to_color[element] = tuple(map(int, row['BGRColor'].strip('()').split(', ')))
-
-                                                                                                                                                                                                                                                        
-
-# Not used at this time
-def reverse_lookup(dictionary, value):
-    """Find the dictionary key for the given value."""
-    for key, val in dictionary.items():
-        if val == value:
-            return key
-    return None  # Return None if the value is not found in any key-value pair
-
+            for row in csv_reader:
+                element = row['Element'].lower().replace(" ", "")
+                matte_char_to_color[element] = tuple(map(int, row['BGRColor'].strip('()').split(', ')))
+    else:
+        print(f"{csv_file_path} does not exist. Please supply a valid csv file to map matte colors to elements.")
+        sys.exit(1)
 
 
 def get_files_in_path(colormatte_path):
@@ -59,12 +52,12 @@ def get_frame_number(filename):
         print("Could not extract frame number.")
 
 
-
 def get_frame_range(folder_path):
     """Get the min and max frame numbers for the files in the given path."""
     frame_numbers = []
 
-    # looks for a sequence of digits (\d+) followed by ".png" and ensures that it is a whole word using word boundaries (\b)
+    # looks for a sequence of digits (\d+) followed by ".png" and ensures that it is a whole word
+    # using word boundaries (\b)
     pattern = re.compile(r'\b(\d+)\.png\b')
 
     for filename in os.listdir(folder_path):
@@ -72,10 +65,7 @@ def get_frame_range(folder_path):
         if match:
             frame_numbers.append(int(match.group(1)))
 
-    if frame_numbers:
         return min(frame_numbers), max(frame_numbers)
-    else:
-        return None, None
 
 
 def create_black_matte_filename(existing_file, frame_number):
@@ -87,12 +77,10 @@ def create_black_matte_filename(existing_file, frame_number):
     return new_file    
 
 
-
 def get_image_width_height(image):
     """Given an image, obtain it's width and height."""
     height, width, channels = image.shape
     return width, height
-
 
 
 def get_matte_path(colormatte_path):
@@ -132,7 +120,6 @@ def get_matte_filename(image_path):
     return filename_without_extension
 
 
-
 def get_matte_colors(image):
     """Get the list of colors that were used for mattes in the given image."""
     flattened_image = image.reshape((-1, 3))
@@ -142,7 +129,6 @@ def get_matte_colors(image):
     black = (0, 0, 0)
     unique_colors_list.remove(black)
     return unique_colors_list
-
 
 
 def get_char_bw_matte_from_colormatte(image, target_color):
@@ -182,13 +168,13 @@ def create_frame_dict(files):
     return frame_dict
 
 
-# add comment
-
 parser = argparse.ArgumentParser(description='Create black and white mattes for elements in color mattes.')
 
 # Required parameters
+parser.add_argument('map_colors_path', type=str, help='Path to csv file that maps matte colors to elements')
 parser.add_argument('colormatte_path', help='Path to the colormatte files from which to extract black and white mattes')
-parser.add_argument('elements', type=str, help='Comma separated (no spaces, no brackets) list of elements that need black and white mattes. Example: element1,element2')
+parser.add_argument('elements', type=str, help='Comma separated (no spaces, no brackets) list of elements that need'
+                                               ' black and white mattes. Example: element1,element2')
 
 # Optional parameters
 parser.add_argument('-sf', '--start_frame', type=int, help='Start processing at this frame')
@@ -200,26 +186,27 @@ args = parser.parse_args()
 bw_matte_elements = args.elements.split(',')
 
 
-# Get the frame range from the denoise path
+# Get the default frame range from the denoise path
 denoise_path = get_denoise_path(args.colormatte_path)
 
-if args.start_frame == None or args.end_frame == None:
+if args.start_frame is None or args.end_frame is None:
     denoise_start_frame, denoise_end_frame = get_frame_range(denoise_path)
     print('denoise_start_frame', denoise_start_frame)
     print('denoise_end_frame', denoise_end_frame)
 
-if args.start_frame == None:
+if args.start_frame is None:
     matte_start_frame = denoise_start_frame
 else:
     matte_start_frame = args.start_frame
 
 
-if args.end_frame == None:
+if args.end_frame is None:
     matte_end_frame = denoise_end_frame
 else:
     matte_end_frame = args.end_frame
 
 
+print('map_colors_path', args.map_colors_path)
 print('colormatte_path', args.colormatte_path)
 print('elements', bw_matte_elements)
 print('start_frame', matte_start_frame)
@@ -229,7 +216,7 @@ print('increment', args.increment)
 matte_path = get_matte_path(args.colormatte_path)
 
 # Use the csv file to create a matte color lookup by character
-populate_char_to_color_dict()
+populate_char_to_color_dict(args.map_colors_path)
 
 
 invalid_elements = []
